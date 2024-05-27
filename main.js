bde53252176632831c5bd2d6e0b83bbe39c38152
Alisha7295 <168325700+Alisha7295@users.noreply.github.com>
2024-05-27 20:31:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { readdirSync, readFileSync, writeFileSync } = require("fs-extra");
-const { join, resolve } = require('path')
+const { join, resolve } = require('path');
 const { execSync } = require('child_process');
 const config = require("./config.json");
 const chalk = require("chalk");
@@ -8,6 +8,7 @@ const listPackage = JSON.parse(readFileSync('./package.json')).dependencies;
 const fs = require("fs");
 const moment = require("moment-timezone");
 const logger = require("./utils/log.js");
+const express = require('express'); // Express server ke liye
 
 global.client = new Object({
     commands: new Map(),
@@ -21,24 +22,24 @@ global.client = new Object({
     configPath: new String(),
     getTime: function (option) {
         switch (option) {
-        case "seconds":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("ss")}`;
-        case "minutes":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("mm")}`;
-        case "hours":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("HH")}`;
-        case "date":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("DD")}`;
-        case "month":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("MM")}`;
-        case "year":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("YYYY")}`;
-        case "fullHour":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("HH:mm:ss")}`;
-        case "fullYear":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("DD/MM/YYYY")}`;
-        case "fullTime":
-            return `${moment.tz("Asia/Ho_Chi_minh").format("HH:mm:ss DD/MM/YYYY")}`;
+            case "seconds":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("ss")}`;
+            case "minutes":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("mm")}`;
+            case "hours":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("HH")}`;
+            case "date":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("DD")}`;
+            case "month":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("MM")}`;
+            case "year":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("YYYY")}`;
+            case "fullHour":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("HH:mm:ss")}`;
+            case "fullYear":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("DD/MM/YYYY")}`;
+            case "fullTime":
+                return `${moment.tz("Asia/Ho_Chi_minh").format("HH:mm:ss DD/MM/YYYY")}`;
         }
     },
     timeStart: Date.now()
@@ -86,14 +87,15 @@ try {
     for (const key in configValue) global.config[key] = configValue[key];
     logger.loader("Config Loaded!");
 } catch (e) {
-    return logger.loader("Can't load file config!", "error")
+    return logger.loader("Can't load file config!", "error");
 }
 
 for (const property in listPackage) {
     try {
-        global.nodemodule[property] = require(property)
-    } catch (e) {}
+        global.nodemodule[property] = require(property);
+    } catch (e) { }
 }
+
 const langFile = (readFileSync(`${__dirname}/languages/${global.config.language || "en"}.lang`, {
     encoding: 'utf-8'
 })).split(/\r?\n|\r/);
@@ -118,14 +120,14 @@ global.getText = function (...args) {
         text = text.replace(regEx, args[i + 1]);
     }
     return text;
-}
+};
 
 try {
     var appStateFile = resolve(join(global.client.mainPath, config.APPSTATEPATH || "appstate.json"));
     var appState = ((process.env.REPL_OWNER || process.env.PROCESSOR_IDENTIFIER) && (fs.readFileSync(appStateFile, 'utf8'))[0] != "[" && config.encryptSt) ? JSON.parse(global.utils.decryptState(fs.readFileSync(appStateFile, 'utf8'), (process.env.REPL_OWNER || process.env.PROCESSOR_IDENTIFIER))) : require(appStateFile);
-    logger.loader("Found the bot's appstate file.")
+    logger.loader("Found the bot's appstate file.");
 } catch (e) {
-    return logger.loader("Can't find the bot's appstate file.", "error")
+    return logger.loader("Can't find the bot's appstate file.", "error");
 }
 
 function onBot() {
@@ -134,11 +136,11 @@ function onBot() {
     login(loginData, async (loginError, loginApiData) => {
         if (loginError) {
             if (loginError.error == 'Error retrieving userID. This can be caused by a lot of things, including getting blocked by Facebook for logging in from an unknown location. Try logging in with a browser to verify.') {
-                console.log(loginError.error)
-                process.exit(0)
+                console.log(loginError.error);
+                process.exit(0);
             } else {
-                console.log(loginError)
-                return process.exit(0)
+                console.log(loginError);
+                return process.exit(0);
             }
         }
         console.log(chalk.blue(`============== LOGIN BOT ==============`));
@@ -148,13 +150,13 @@ function onBot() {
         d = JSON.stringify(d, null, '\x09');
         if ((process.env.REPL_OWNER || process.env.PROCESSOR_IDENTIFIER) && global.config.encryptSt) {
             d = await global.utils.encryptState(d, process.env.REPL_OWNER || process.env.PROCESSOR_IDENTIFIER);
-            writeFileSync(appStateFile, d)
+            writeFileSync(appStateFile, d);
         } else {
-            writeFileSync(appStateFile, d)
+            writeFileSync(appStateFile, d);
         }
         global.account.cookie = fbstate.map(i => i = i.key + "=" + i.value).join(";");
-        global.client.api = loginApiData
-        global.config.version = config.version,
+        global.client.api = loginApiData;
+        global.config.version = config.version;
         (async () => {
             const commandsPath = `${global.client.mainPath}/modules/commands`;
             const listCommand = readdirSync(commandsPath).filter(command => command.endsWith('.js') && !command.includes('example') && !global.config.commandDisabled.includes(command));
@@ -199,7 +201,7 @@ function onBot() {
                             global.configModule[moduleName][envConfigKey] = global.config[moduleName][envConfigKey] ?? envConfig[envConfigKey];
                             global.config[moduleName][envConfigKey] = global.config[moduleName][envConfigKey] ?? envConfig[envConfigKey];
                         }
-                        var configPath = require('./config.json');
+                                                var configPath = require('./config.json');
                         configPath[moduleName] = envConfig;
                         writeFileSync(global.client.configPath, JSON.stringify(configPath, null, 4), 'utf-8');
                     }
@@ -309,3 +311,13 @@ function onBot() {
         global.loading.err(`${chalk.hex('#ff7100')(`[ CONNECT ]`)} Cannot connect to the JSON database.`, "DATABASE");
     }
 })();
+
+// Server setup for Render deployment
+const app = express();
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+    res.send('Bot is running');
+});
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
